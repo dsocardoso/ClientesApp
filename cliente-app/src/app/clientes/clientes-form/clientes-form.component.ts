a8a10e08b6ec2b8b14a51eaa7cf30089b148b9ff
Diego Semensato O Cardoso <dsocardoso@gmail.com>
2021-05-17 09:00:16 -0300

    Cadastro e Listagem dos clientes

---
+++ cliente-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Cliente } from '../cliente';
+
+@Component({
+  selector: 'app-clientes-form',
+  templateUrl: './clientes-form.component.html',
+  styles: [
+  ]
+})
+export class ClientesFormComponent implements OnInit {
+
+  cliente: Cliente;
+
+  constructor() {
+    this.cliente = new Cliente();
+   }
+
+  ngOnInit(): void {
+  }
+
+  clicar(){
+    console.log(this.cliente);
+  }
+
+}
